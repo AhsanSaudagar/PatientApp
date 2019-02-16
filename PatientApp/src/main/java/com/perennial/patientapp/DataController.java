b6308433dcf80cdac4a4b82056a5cd34c27d6ee3
ganesh.takale <ganesh.takale@perennialsys.com>
2019-02-16 18:09:29 +0530

    new changes

--- PatientApp/src/main/java/com/perennial/patientapp/DataController.java
+++ PatientApp/src/main/java/com/perennial/patientapp/DataController.java
@@ -1,6 +1,9 @@
 package com.perennial.patientapp;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
