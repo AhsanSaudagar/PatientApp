409144a67d909dd5e705797cfec3fcada0407c6a
Mohasin Kazi <mohasin.kazi@perennialsys.com>
2019-02-16 17:57:12 +0530

    Removed unwanted files

--- PatientApp/src/main/java/com/perennial/patientapp/vo/PatientVO.java
+++ PatientApp/src/main/java/com/perennial/patientapp/vo/PatientVO.java
@@ -35,9 +35,81 @@ public class PatientVO {
 	private String guardianMobileNumber;
 
 
-	@Column(name = "GUARDIAN_NAME")
-	private String guardianName;
+	@Column(name = "ADDRESS")
+	private String address;
 
+	@Column(name = "IS_ACTIVE")
+	private boolean isActive;
 
+	public long getId() {
+		return id;
+	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public List<String> getDiseases() {
+		return diseases;
+	}
+
+	public void setDiseases(List<String> diseases) {
+		this.diseases = diseases;
+	}
+
+	public String getGuardianName() {
+		return guardianName;
+	}
+
+	public void setGuardianName(String guardianName) {
+		this.guardianName = guardianName;
+	}
+
+	public String getGuardianMobileNumber() {
+		return guardianMobileNumber;
+	}
+
+	public void setGuardianMobileNumber(String guardianMobileNumber) {
+		this.guardianMobileNumber = guardianMobileNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean active) {
+		isActive = active;
+	}
 }
