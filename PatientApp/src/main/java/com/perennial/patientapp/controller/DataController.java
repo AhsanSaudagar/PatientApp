8e29c96b9d1c98f51f9b46305f83879fbfb4b566
Mohasin Kazi <mohasin.kazi@perennialsys.com>
2019-02-16 20:47:35 +0530

    added autowired dependancy

--- PatientApp/src/main/java/com/perennial/patientapp/controller/DataController.java
+++ PatientApp/src/main/java/com/perennial/patientapp/controller/DataController.java
@@ -1,18 +1,32 @@
 package com.perennial.patientapp.controller;
 
+import com.perennial.patientapp.handler.DataHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(value="patient")
 public class DataController {
 
+
+    @Autowired
+    private DataHandler dataHandler;
+
     @RequestMapping(value = "/addSchedule", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE)
     public void addMedicineSchedule(long patientId){
 
+    }
+
+
+    @RequestMapping(value = "getUserDetails", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public String saveKpiData(@RequestParam("id") int id, HttpServletRequest request) {
+
+
+        return null;
     }
 
 }
--- PatientApp/src/main/java/com/perennial/patientapp/handler/DataHandler.java
+++ PatientApp/src/main/java/com/perennial/patientapp/handler/DataHandler.java
@@ -1,5 +1,12 @@
 package com.perennial.patientapp.handler;
 
+import com.perennial.patientapp.DAO.PatientAppDAOImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DataHandler implements IDataHandler {
+
+    @Autowired
+    private PatientAppDAOImpl salesDAO;
+
 
 }
